815094356815209bbc24a5398c7934372a900770
Nicolas VINCENT <ni.vincent@student.helmo.be>
2022-12-26 16:57:05 +0100

    Starting the bot

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,24 @@
-import Repository.TomlRepository;
+import repository.TomlRepository;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
+        try{
+            TomlRepository repository = new TomlRepository();
 
-        TomlRepository repository = new TomlRepository();
+            JDA jda = JDABuilder.createDefault(repository.getToken())
+                    .setActivity(Activity.playing("Where is Twab ?"))
+                    .addEventListeners()
+                    .build().awaitReady();
+
+        } catch (InterruptedException e) {
+            System.err.println("[ERROR] : The bot is stop");
+        }
+
 
     }
 }
